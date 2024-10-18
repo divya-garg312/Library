d4636e7a44ece2edb4ced1f78acdf14dd96d70b4
Divya Garg <dgarg2609@gmail.com>
2024-10-18 11:35:17 +0530

    Added DOM Functionality

--- index.js
+++ index.js
@@ -1,58 +1,71 @@
-let show=document.getElementById("show");
-let container=document.getElementById("container");
-let title=document.getElementById("title");
-let author=document.getElementById("author");
-let pages=document.getElementById("pages");
-let add=document.getElementById("add");
-let table=document.getElementById("result");
-let list=document.getElementById("list");
-let bookStatus=document.getElementById("bookStatus");
-const myLibrary=[];
+let show = document.getElementById("show");
+let container = document.getElementById("container");
+let title = document.getElementById("title");
+let author = document.getElementById("author");
+let pages = document.getElementById("pages");
+let add = document.getElementById("add");
+let table = document.getElementById("result");
+let list = document.getElementById("list");
+let bookStatus = document.getElementById("bookStatus");
+let myLibrary = [];
 
-function Book(title,author,pages,bookStatus){
-    this.title=title;
-    this.author=author;
-    this.pages=pages;
-    this.bookStatus=bookStatus;
+let books=localStorage.getItem("bookList");
+if(books){
+  myLibrary=JSON.parse(books);
 }
 
-function addBookToLibrary(newBook){
-    let tr=document.createElement("tr");
-    for(let key in newBook){
-        let first=newBook[key];
-        let td=document.createElement("td");
-        td.textContent=first;
-        tr.append(td);
-    }
-    let btn=document.createElement("button");
-    btn.textContent="x";
-    btn.setAttribute("id","dlt");
-    tr.append(btn);
-    list.append(tr);
-    btn.addEventListener("click",()=>{
-        list.removeChild(tr);
-     });
+function Book(title, author, pages, bookStatus) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.bookStatus = bookStatus;
 }
 
-add.addEventListener("click",()=>{
-    let newBook=new Book(title.value,author.value,pages.value,bookStatus.value);
-    if(title.value!="" && author.value!="" && pages.value!=""){
+function addBookToLibrary(newBook) {
+  let tr = document.createElement("tr");
+  for (let key in newBook) {
+    let first = newBook[key];
+    let td = document.createElement("td");
+    td.textContent = first;
+    tr.append(td);
+  }
+  let btn = document.createElement("button");
+  btn.textContent = "x";
+  btn.setAttribute("id", "dlt");
+  tr.append(btn);
+  list.append(tr);
+  btn.addEventListener("click", () => {
+    list.removeChild(tr);
+    myLibrary=myLibrary.filter(item=> item!=newBook);
+    localStorage.setItem("bookList",JSON.stringify(myLibrary));
+  });
+}
+
+add.addEventListener("click", () => {
+  let newBook = new Book(title.value, author.value, pages.value, bookStatus.value);
+  if (title.value != "" && author.value != "" && pages.value != "") {
     myLibrary.push(newBook);
+    let jsonData = JSON.stringify(myLibrary);
+    localStorage.setItem("bookList", jsonData);
     addBookToLibrary(newBook);
-    }
-    else{
-      alert("Enter all the fields of input");
-    }
+  }
+  else {
+    alert("Enter all the fields of input");
+  }
 })
 
-show.addEventListener("click",()=>{
-  if(show.textContent!="Hide Form"){
-    container.style.display="flex";
-    show.textContent="Hide Form";
+show.addEventListener("click", () => {
+  if (show.textContent != "Hide Form") {
+    container.style.display = "flex";
+    show.textContent = "Hide Form";
   }
-  else{
-    container.style.display="none";
-    show.textContent="Add New Book";
+  else {
+    container.style.display = "none";
+    show.textContent = "Add New Book";
   }
+});
+
+myLibrary.forEach(value => {
+  addBookToLibrary(value);
 });
 
